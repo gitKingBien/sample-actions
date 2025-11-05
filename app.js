12c6760bbd3cb79bd4fbeccccb0278ba73f72202
Bien <bien@sitesphil.com>
2025-11-05 18:17:54 +0800

    Initial simple Node PoC

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.end('Hello from Simple DevSecOps PoC!');
+}).listen(port, () => {
+  console.log('Server running on port', port);
+});
